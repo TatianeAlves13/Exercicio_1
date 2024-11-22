3e240938902105e7251d98b290e15f207e49ab5e
TatianeAlves13 <alves1395.thatyanne@gmail.com>
2024-11-21 21:12:54 -0300

    Exercício de Tabuada

---
+++ TabuadaRevisão.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero para ver a tabuada: ");
+            int numero = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"\nTabuada do {numero: D3}:");
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"\n{numero:D3} x {i:D3} = {(numero * i):D3}");
+            }
+
+            Console.ReadLine();
+
+
+        }
+
+
+
+
+
+    }
+}
